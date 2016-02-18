da76a102c74ddd7d501f4defe161610639d70fa1
Traun Leyden <tleyden@couchbase.com>
2016-02-18 15:49:31 -0800

    Build script installs repo if needed

--- build.sh
+++ build.sh
@@ -1,9 +1,30 @@
 #!/bin/sh -e
 
-# This script builds the sync gateway.
+# This script builds sync gateway using pinned dependencies.
 
-# Set the git commit info before the build
+## Make sure the repo tool is installed, otherwise throw an error
+which repo
+if (( $? != 0 )); then
+    echo "You must install the repo tool first.  Try running: "
+    echo "sudo curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/bin/repo"
+    exit 1
+fi
+
+## If we don't already have a .repo directory, run "repo init"
+REPO_DIR=.repo
+if [ ! -d "$REPO_DIR" ]; then
+    echo "No .repo directory found, running 'repo init'"
+    repo init -u "https://github.com/couchbase/sync_gateway.git" -m manifest/default.xml
+fi
+
+## Repo Sync
+repo sync
+
+## Update the version
+SG_DIR=`pwd`/godeps/src/github.com/couchbase/sync_gateway
+CURRENT_DIR=`pwd`
 BUILD_INFO="./rest/git_info.go"
+cd $SG_DIR
 
 #tell git to ignore any local changes to git_info.go, we don't want to commit them to the repo
 git update-index --assume-unchanged ${BUILD_INFO}
@@ -18,6 +39,10 @@ sed -i.bak -e 's/GitCommit.*=.*/GitCommit = "'$GIT_COMMIT'"/' $BUILD_INFO
 sed -i.bak -e 's/GitBranch.*=.*/GitBranch = "'$GIT_BRANCH'"/' $BUILD_INFO
 sed -i.bak -e 's/GitDirty.*=.*/GitDirty = "'$GIT_DIRTY'"/' $BUILD_INFO
 
-./go.sh install "$@" -v ./...
+# Go back to previous dir
+cd $CURRENT_DIR
 
-echo "Success! Output is $GOPATH/bin/sync_gateway and $GOPATH/bin/sg_accel"
+## Go Install
+GOPATH=`pwd`/godeps go install github.com/couchbase/sync_gateway/...
+
+echo "Success! Output is godeps/bin/sync_gateway and godeps/bin/sg_accel"
